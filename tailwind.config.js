da9b9f15eeb796e8cf0832d740069d99a68e393f
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-09 01:59:52 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        bg: '#F7F7F8',
+        catBg:'#45485F',
+      }
+    },
+  },
+  plugins: [],
+}
